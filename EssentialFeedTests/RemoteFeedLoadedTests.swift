746241c46aad9d828d16cd4cf46a84457cd1b09b
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2022-08-07 12:54:31 +0800

    feat: initiate RemoteFeedLoaderTests

---
+++ EssentialFeedTests/RemoteFeedLoadedTests.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoadedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Uwais Alqadri on 8/7/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+
+}
+
+class HTTPClient {
+	var requestURL: URL?
+}
+
+class RemoteFeedLoadedTests: XCTestCase {
+
+	func test_init_doesNotRequestDataFromURL() {
+		let client = HTTPClient()
+		_ = RemoteFeedLoader()
+
+		XCTAssertNil(client.requestURL)
+	}
+
+}
