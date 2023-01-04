9686f906ec548a2070c0d4dfb9921f5c7ffb385b
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2023-01-04 21:35:29 +0800

    feat: save command fails on cache deletion error

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -18,8 +18,9 @@ class LocalFeedLoader {
 		self.currentDate = currentDate
 	}
 
-	func save(_ items: [FeedItem]) {
+	func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void = { _ in }) {
 		store.deleteCachedFeed { [unowned self] error in
+			completion(error)
 			if error == nil {
 				self.store.insert(items, timestamp: currentDate())
 			}
@@ -94,6 +95,24 @@ class CacheFeedUseCaseTests: XCTestCase {
 		store.completeDeletionSuccessfully()
 
 		XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+	}
+
+	func test_save_failsOnDeletionError() {
+		let items = [uniqueItem(), uniqueItem()]
+		let (sut, store) = makeSUT()
+		let deletionError = anyNSError()
+		let exp = expectation(description: "Wait for save completion")
+
+		var receivedError: Error?
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+
+		store.completeDeletion(with: deletionError)
+		wait(for: [exp], timeout: 1.0)
+
+		XCTAssertEqual(receivedError as? NSError, deletionError)
 	}
 
 	// MARK: -- Helpers
