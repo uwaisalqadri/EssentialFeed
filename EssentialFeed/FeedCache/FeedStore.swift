b8a1c22a982480c7d00495284c6d7b8bd04a0d31
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2023-01-07 22:42:00 +0800

    refactor: move FeedStore to its own file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Uwais Alqadri on 1/7/23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+	typealias DeletionCompletion = (Error?) -> Void
+	typealias InsertionCompletion = (Error?) -> Void
+
+	func deleteCachedFeed(completion: @escaping DeletionCompletion)
+	func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
