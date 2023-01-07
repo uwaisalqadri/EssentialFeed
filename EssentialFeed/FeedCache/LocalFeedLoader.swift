423abb3c8bd2342f1a5d3774777ca6a16ae8f15b
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2023-01-07 23:00:59 +0800

    refactor: add `LocalFeedItem` data transfer representation to decouple storage frameworks from `FeedItem` data models

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -32,9 +32,17 @@ public class LocalFeedLoader {
 	}
 
 	private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
-		store.insert(items, timestamp: currentDate()) { [weak self] error in
+		store.insert(items.toLocal(), timestamp: currentDate()) { [weak self] error in
 			guard self != nil else { return }
 			completion(error)
+		}
+	}
+}
+
+public extension Array where Element == FeedItem {
+	func toLocal() -> [LocalFeedItem] {
+		return self.map {
+			LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)
 		}
 	}
 }
